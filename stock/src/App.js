9b20fa41c02d36ad96fcf2d4ee1d343eff8e7a12
MatheusFPalma <matheusfpalma@hotmail.com>
2023-06-06 15:25:30 -0300

    Atualização layout

--- stock/src/App.js
+++ stock/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './components/pages/Home';
+import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Container customClass='min-height'><Home /></Container>} />
+      </Routes>
+      <Footer/>
+    </Router>
+  )
 }
 
 export default App;
